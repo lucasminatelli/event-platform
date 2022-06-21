4fe314c0984a14d8541f1638a2963a2c54effb93
Lucas Rezende <lucas.minatelli@gmail.com>
2022-06-21 09:41:32 -0300

    fix: uri

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: '',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o4enp10h9v01xxhksp8wyl/master',
     cache: new InMemoryCache()
 })
